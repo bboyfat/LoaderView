ff3849ff4b92d5f0f783e6be247c5be91b6c50fe
Andrii_Petrovskyi <>
2021-12-03 12:13:16 +0200

    - initial commit

---
+++ Sources/LoaderView/LoaderView.swift
@@ -0,0 +1,6 @@
+public struct LoaderView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LoaderViewTests/LoaderViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LoaderView
+
+final class LoaderViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LoaderView().text, "Hello, World!")
+    }
+}
